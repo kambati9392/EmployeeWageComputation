a2515741d8d541252ae618b53d674533b41442c2
Rammohan <reddy.kambati@gmail.com>
2024-09-11 09:42:28 +0530

    ADDED functionality calculation  daily wages

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,14 +3,25 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
-		 int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-	        if(empCheck == 0)
-	        {
-	            System.out.println("Employee is Present");
-	        }
-	        else
-	        {
-	            System.out.println("Employee is Absent");
-	        }
+		int wagePerHour = 20;
+        int fullDay = 8;
+        int halfDay = 4;
+
+        System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+        System.out.println("");
+
+        // Generate a random number between 0 and 2
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+        if (empCheck == 0) {
+            System.out.println("Employee is Present for a Full Day");
+            System.out.println("The daily wage of Employee is : " + (wagePerHour * fullDay));
+        } else if (empCheck == 1) {
+            System.out.println("Employee is Present but working part time");
+            System.out.println("The daily wage of Employee is : " + (wagePerHour * halfDay));
+        } else {
+            System.out.println("Employee is Absent");
+            System.out.println("The daily wage of Employee is : 0");
+        }
 	}
 }
